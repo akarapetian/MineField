1644249c9f7eac440b0c99d0e42aa9146056f3ce
Sahen Rai <sahen.rai@gmail.com>
2021-11-12 06:30:50 -0800

    Added menu stuff

--- main-scene.js
+++ main-scene.js
@@ -8,6 +8,7 @@ import {Surfaces_Demo} from "./examples/surfaces-demo.js"
 import {Text_Demo} from "./examples/text-demo.js"
 import {Transforms_Sandbox} from "./examples/transforms-sandbox.js"
 import {minefield} from "./minefield.js";
+import {StartScreen} from "./StartScreen.js";
 
 // Pull these names into this module's scope for convenience:
 const {
@@ -34,14 +35,15 @@ Object.assign(defs,
     {Surfaces_Demo},
     {Text_Demo},
     {Transforms_Sandbox},
-    {minefield}
+    {minefield},
+     {StartScreen}
 );
 
 // ******************** End extra step
 
 // (Can define Main_Scene's class here)
 
-const Main_Scene = minefield;
+var Main_Scene = StartScreen;
 const Additional_Scenes = [];
 
 export {Main_Scene, Additional_Scenes, Canvas_Widget, Code_Widget, Text_Widget, defs}
