17267c66763cc80c93ed15827520d48090a73410
shrey-ucla <shreyas12345@ucla.edu>
2021-11-11 19:37:11 -0800

    underwater background

--- minefield.js
+++ minefield.js
@@ -2,7 +2,7 @@ import {defs, tiny} from './examples/common.js';
 import { Shape_From_File } from './examples/obj-file-demo.js';
 
 const {
-    Vector, Vector3, vec, vec3, vec4, color, hex_color, Shader, Matrix, Mat4, Light, Shape, Material, Scene,
+    Vector, Vector3, vec, vec3, vec4, color, hex_color, Shader, Matrix, Mat4, Light, Shape, Material, Scene, Texture
 } = tiny;
 
 export class minefield extends Scene {
@@ -15,6 +15,11 @@ export class minefield extends Scene {
         const row_operation = (s, p) => p ? Mat4.translation(0, .2, 0).times(p.to4(1)).to3()
             : initial_corner_point;
         const column_operation = (t, p) => Mat4.translation(.2, 0, 0).times(p.to4(1)).to3();
+
+        const initial_corner_point2 = vec3(-30, -19, -20);
+        const row_operation2 = (s, p) => p ? Mat4.translation(0, .2, 0).times(p.to4(1)).to3()
+            : initial_corner_point2;
+        const column_operation2 = (t, p) => Mat4.translation(.2, 0, 0).times(p.to4(1)).to3();
         
         this.shapes = {
             torus: new defs.Torus(15, 15),
@@ -23,17 +28,27 @@ export class minefield extends Scene {
             circle: new defs.Regular_2D_Polygon(1, 15),
             cylinder: new Shape_From_File("assets/sub.obj"),
             cube: new defs.Cube(3,3),
-            sheet: new defs.Grid_Patch(100, 500, row_operation, column_operation),
+            horizon: new defs.Grid_Patch(100, 500, row_operation, column_operation),
+            ground: new defs.Grid_Patch(100, 300, row_operation2, column_operation2)
         };
 
         // *** Materials
+        const bump = new defs.Fake_Bump_Map(1);
         this.materials = {
             test: new Material(new defs.Phong_Shader(),
-                {ambient: .4, specularity: 1, diffusivity: .6, color: hex_color("#ffffff")}),
+                {ambient: .4, specularity: 1, diffusivity: .6, color: hex_color("#000000")}),
+            // horizon: new Material(new defs.Phong_Shader(),
+            //     {ambient: 0.2, specularity: 1, diffusivity: .6, color: hex_color("#ADD8E6")}),
+            horizon: new Material(bump, {ambient: 1, texture: new Texture("assets/underwater.jpg")}),
+            // ground: new Material(new defs.Phong_Shader(),
+            //     {ambient: 1, specularity: 1, diffusivity: .6, color: hex_color("#ffffff")}),
+            // ground: new Material(bump, {ambient: 1, texture: new Texture("assets/sand.jpg")}),
         }
 
         this.initial_camera_location = Mat4.look_at(vec3(0, 2, 13), vec3(0, 0, 0), vec3(0, 1, 0));
         this.player_matrix = Mat4.identity().times(Mat4.scale(2,2,2)).times(Mat4.rotation(Math.PI,0,1,0)).times(Mat4.translation(0,0,-3));
+        this.horizon_matrix = Mat4.identity();
+        this.ground_matrix = Mat4.identity();
         this.context = null;
         this.program_state = null;
         this.bullets = []
@@ -41,11 +56,12 @@ export class minefield extends Scene {
     }
 
     move_left() {
-            this.player_matrix = this.player_matrix.times(Mat4.translation(1,0,0));
+            this.player_matrix = this.player_matrix.times(Mat4.translation(0.2,0,0));
     }
 
     move_right() {
-            this.player_matrix = this.player_matrix.times(Mat4.translation(-1,0,0));
+            this.player_matrix = this.player_matrix.times(Mat4.translation(-0.2,0,0));
+            // this.background_matrix = this.background_matrix.times(Mat4.rotation(0.2,0.2,0,0));
     }
 
     fire_bullet() {
@@ -78,6 +94,9 @@ export class minefield extends Scene {
         // TODO:  Fill in matrix operations and drawing code to draw the solar system scene (Requirements 3 and 4)
         const t = program_state.animation_time / 1000, dt = program_state.animation_delta_time / 1000;
         let model_transform = this.player_matrix;
+        var horizon_transform = Mat4.identity().times(Mat4.scale(150, 50, 1)).times(Mat4.translation(0,0,-20));
+        // this.ground_matrix = Mat4.identity().times(Mat4.scale(18, 5, 1)).times(Mat4.translation(0,-2,-10).times(Mat4.rotation(3,1,0,0)));
+
         // if(t % 10 == 0){
         //     this.obstacles.push(this.player_matrix.times(Mat4.scale(0.1,0.1,0.1)));
         // }
@@ -88,6 +107,9 @@ export class minefield extends Scene {
         program_state.lights = [new Light(light_position, color(0, 0, 0, 1), 10000)]
 
         this.shapes.cylinder.draw(context, program_state, model_transform, this.materials.test)
+        this.shapes.cube.draw(context, program_state, horizon_transform, this.materials.horizon)
+        // this.shapes.cube.draw(context, program_state, this.ground_matrix, this.materials.ground)
+
         for(let i = 0; i < this.bullets.length; i++){
             this.bullets[i] = this.bullets[i].times(Mat4.translation(0,0,1));
             this.shapes.cube.draw(context, program_state, this.bullets[i], this.materials.test);
@@ -96,7 +118,8 @@ export class minefield extends Scene {
             this.obstacles[i] = this.obstacles[i].times(Mat4.translation(0,0,1));
             this.shapes.cube.draw(context, program_state, this.obstacles[i], this.materials.test);
         }
-        this.shapes.sheet.draw(context, program_state, Mat4.identity(), this.materials.test);
+        // this.shapes.horizon.draw(context, program_state, this.horizon_matrix, this.materials.horizon);
+        // this.shapes.ground.draw(context, program_state, this.ground_matrix, this.materials.ground);
 
     }
 }
